ed7ba861509d127ce8faf3849188d2c2108b1905
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-07-29 11:25:09 +1000

    changed user relation annotation with state model
    has_many:  => has_one

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :city, :through => :location
-  has_many :state, :through => :city
+  has_one :state, :through => :city
         
   has_many :received_user_ratings, :class_name => 'UserRating', :foreign_key => 'reviewee_id'
   has_many :sent_user_ratings, :class_name => 'UserRating', :foreign_key => 'reviewer_id'

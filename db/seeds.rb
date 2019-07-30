46b239ab9c5699c44a4cad74867d56d9b114a6a1
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-07-30 18:46:00 +1000

    states, cites and locations seed data. Location requires modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+states = [
+    {
+        state: "Victoria",
+    },
+    {
+        state: "Queensland",
+    },
+    {
+        state: "New South Wales"
+    },
+    {
+        state: "Western Australia"
+    },
+    {
+        state: "South Australia"
+    },
+    {
+        state: "Northern Territory"
+    },
+    {
+        state: "Tasmania"
+    },
+    {
+        state: "Australain Capital Territory"
+    }
+]
+
+states.each do |state|
+    State.create(state)
+end
+
+cities = [
+    {
+        city: "Melbourne",
+    },
+    {
+        city: "Brisbane",
+    },
+    {
+        city: "Sydney"
+    },
+    {
+        city: "Perth"
+    },
+    {
+        city: "Adelaide"
+    },
+    {
+        city: "Darwin"
+    },
+    {
+        city: "Hobart"
+    },
+    {
+        city: "Canberra"
+    }
+]
+
+cities.each do |city|
+    City.create(city)
+end
+
+locations = [
+    {
+        postcode: 3141,
+        suburb: "South Yarra",
+        city_id: 1
+    },
+    {
+        postcode: 4169,
+        suburb: "Kangaroo Point",
+        city_id: 2
+    },
+    {
+        postcode: 2026,
+        suburb: "Bondi",
+        city_id: 3
+    },
+    {
+        postcode: 6153,
+        suburb: "Applecross",
+        city_id: 4
+    },
+    {
+        postcode: 5015,
+        suburb: "Port Adelaide",
+        city_id: 5
+    },
+    {
+        postcode: 0810,
+        suburb: "Coconut Grove",
+        city_id: 6
+    },
+    {
+        postcode: "Hobart",
+        suburb: "South Yarra",
+        city_id: "1"
+    },
+    {
+        postcode: "Canberra",
+        suburb: "South Yarra",
+        city_id: "1"
+    }
+]
+
+locations.each do |location|
+    Location.create(location)
+end

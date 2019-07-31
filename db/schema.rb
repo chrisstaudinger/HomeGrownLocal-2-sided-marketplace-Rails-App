580f16eb06b7c004e0b56aa7853f33e68c51835b
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-07-31 14:07:08 +1000

    migration to remove name column from roles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_024032) do
+ActiveRecord::Schema.define(version: 2019_07_31_040548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2019_07_31_024032) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string "name"
     t.string "privilege"
     t.text "description"
     t.datetime "created_at", null: false

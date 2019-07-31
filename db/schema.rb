afc92a2fb38118bbe935cfccd750e4d8f5832a47
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-07-31 12:39:10 +1000

    Created a migration for a measurements table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_021239) do
+ActiveRecord::Schema.define(version: 2019_07_31_023323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,12 @@ ActiveRecord::Schema.define(version: 2019_07_31_021239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_locations_on_city_id"
+  end
+
+  create_table "measurements", force: :cascade do |t|
+    t.string "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|

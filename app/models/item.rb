16779b1781c91295a3f4ce67c270bbc8f042ab39
selly santoso <santoso.sally22@gmail.com>
2019-08-05 17:49:28 +1000

    minor changes, fixed two merge conflict problems

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,6 @@ class Item < ApplicationRecord
   has_many :requests
   has_many :orders, :through => :requests
   has_many :item_reviews
-<<<<<<< HEAD
-=======
 
   has_one_attached :image
->>>>>>> 617984cb964b62a059387f6a530eee9b5ad7615e
 end

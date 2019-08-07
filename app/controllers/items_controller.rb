669ef7273b16ee99480122b123e11954aeed15cc
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-08-07 12:53:23 +1000

    front-end display changes for user buying process. fixed a model association. quantity purchase form created and rendered

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
   # GET /items/1.json
   def show
     @item = Item.find(params[:id])
+    @dollar_amount = '%.2f' % @item.price
   end
 
   # GET /items/new
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  belongs_to :item
   # has_many :requests
   # has_many :items, :through => :requests
 end

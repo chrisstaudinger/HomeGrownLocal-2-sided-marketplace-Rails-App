ab8a5e8db6f0e96f49794a8f791a63fe2fec0f8c
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-08-08 15:55:35 +1000

    Donation button working. Tested.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @fresh_items = Item.last(6);
+    @fresh_items = Item.last(6)
+    
+    @donation_amount_cents = 500
+    @donation_amount_dollars = @donation_amount_cents / 100
+    @donation_amount_dollars_display = "$#{sprintf('%.2f', @donation_amount_dollars)}"
   end
 end

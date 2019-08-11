fac1b8e0b91079c6d8ccb261bc6791f3c0c93c0e
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-08-11 15:15:21 +1000

    server logs says email is being sent. nothing is received whilst testing on local. Will be testing on heroku.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,11 @@ class UserMailer < ApplicationMailer
     default from: 'homegrownlocalaus@gmail.com'
 
     def welcome_email
-        @user = params[:user]
+        puts "==========================================="
+        puts params[:email]
+
+        @email = params[:email]
         @url = 'https://homegrownlocal.herokuapp.com/'
-        mail(to: @user.email, subject: 'Welcome to Home Grown Local')
+        mail(to: @email, subject: 'Welcome to Home Grown Local')
     end
 end

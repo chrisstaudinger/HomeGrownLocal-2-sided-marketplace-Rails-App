fb1be8f06345b9aa62719ed1c546bedef7e64741
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-08-12 23:54:41 +1000

    deleting unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,13 @@ Rails.application.routes.draw do
   resources :messages
   get 'conversations/my_conversations', to: 'conversations#my_conversations'
   resources :conversations
-  resources :item_reviews
-  resources :measurements
   resources :charges
-  resources :requests
   resources :watch_items
-  resources :orders
   get '/items/search', to: 'items#search'
   get '/items/fresh', to: 'items#fresh'
   resources :items
   resources :watchlists
-  resources :item_categories
-  resources :profiles
-  resources :user_ratings
   devise_for :users
-  resources :locations
-  resources :cities
-  resources :states
-  resources :roles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get '/welcome/about_us', to: 'welcome#about_us'
